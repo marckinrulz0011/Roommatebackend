26cf6e5050dd4e2cdc6e694a4eaa56576e94b47d
JCTR <JCTR@examen.com.pe>
2020-10-01 00:57:09 -0500

    Accomodations implementation

--- Roommates.API/Domain/Models/Accomodation.cs
+++ Roommates.API/Domain/Models/Accomodation.cs
@@ -7,5 +7,14 @@ namespace Roommates.API.Domain.Models
 {
     public class Accomodation
     {
+        public int Id { set; get; }
+        public string Addres { set; get; }
+        public string Description { set; get; }
+        public int NBedrooms { set; get; }
+        public int NBathrooms { set; get; }
+        public int NSquareMeter { set; get;}
+        public int NKitchen { get; set; }
+        public int NRoom { get; set;}
+        public int Price { set; get; }
     }
 }
