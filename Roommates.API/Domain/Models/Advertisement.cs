a24b371c3b72305c5154a94e8e5443277bf85048
JCTR <JCTR@examen.com.pe>
2020-10-01 01:03:05 -0500

    Advertisement implementation

--- Roommates.API/Domain/Models/Advertisement.cs
+++ Roommates.API/Domain/Models/Advertisement.cs
@@ -7,5 +7,10 @@ namespace Roommates.API.Domain.Models
 {
     public class Advertisement
     {
+        public int Id { get; set; }
+        public int NLikes { get; set; }
+        public int NViews { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
     }
 }
