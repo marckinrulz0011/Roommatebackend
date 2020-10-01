acead61a8dfe67153b8f79778649930852ca5613
JCTR <JCTR@examen.com.pe>
2020-10-01 00:23:04 -0500

    Models updated

---
+++ Roommates.API/Domain/Models/Advertisement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roommates.API.Domain.Models
+{
+    public class Advertisement
+    {
+    }
+}
