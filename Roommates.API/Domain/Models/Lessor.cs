83a150a9113eeb2c9746fbbf2572d6b9c9889d9e
Sigi Andre <andrew_programador@hotmail.com>
2020-10-01 00:36:31 -0500

    Lessor

--- Roommates.API/Domain/Models/Lessor.cs
+++ Roommates.API/Domain/Models/Lessor.cs
@@ -7,5 +7,12 @@ namespace Roommates.API.Domain.Models
 {
     public class Lessor
     {
+        public int lessorId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Dni { get; set; }
+        public string Telephone { get; set; }
+        public string sexo { get; set; }
+        public string dateBirthday { get; set; }
     }
 }
