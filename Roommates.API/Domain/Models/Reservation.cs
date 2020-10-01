f9a7e518c20b8f5b1c3e0f14d2ff7be0c3ed3736
JCTR <JCTR@examen.com.pe>
2020-10-01 00:59:16 -0500

    Reservation implementatios 2

--- Roommates.API/Domain/Models/Reservation.cs
+++ Roommates.API/Domain/Models/Reservation.cs
@@ -7,5 +7,9 @@ namespace Roommates.API.Domain.Models
 {
     public class Reservation
     {
+        public int Id { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int Price { get; set; }
     }
 }
